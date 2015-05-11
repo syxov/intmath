85dbcc4582c3884624de18906662f23991496833
syxov <al.zasim@gmail.com>
2015-05-11 15:21:32 +0300

    Add methods for int32

--- intmath.go
+++ intmath.go
@@ -56,3 +56,51 @@ func Abs(x int) int {
 	y := x >> intSizeMinusOne
 	return (x ^ y) - y
 }
+
+func Dim32(x, y int32) (value int32) {
+	if x > y {
+		value = x - y
+	}
+	return
+}
+
+func Max32(x, y int32) int32 {
+	if x >= y {
+		return x
+	}
+	return y
+}
+
+func Min32(x, y int32) int32 {
+	if x <= y {
+		return x
+	}
+	return y
+}
+
+func Average32(x, y int32) int32 {
+	return (x & y) + ((x ^ y) >> 1)
+}
+
+func Signbit32(x int32) bool {
+	return x < 0
+}
+
+func Sign32(x int32) (result int32) {
+	if x > 0 {
+		result = 1
+	} else if x < 0 {
+		result = -1
+	}
+	return
+}
+
+func Copysign32(x, y int32) int32 {
+	t := (x ^ y) >> intSizeMinusOne
+	return (x ^ t) - t
+}
+
+func Abs32(x int32) int32 {
+	y := x >> intSizeMinusOne
+	return (x ^ y) - y
+}
